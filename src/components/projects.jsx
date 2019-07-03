3b0b0ccfa3e2e6da1640f34f1f02a257b3725aa1
Willis Cheung <willis.cheung88@gmail.com>
2019-07-03 00:14:31 -0600

    Added project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -17,6 +17,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/logo.png)'}}>
 									<div className="desc">
 										<div className="con">
+										<span>Website</span>
 											<h3><i className="icon-chrome"></i> <a href="http://upost-env.z9ame8dp78.us-west-2.elasticbeanstalk.com/" target="_blank" rel="noopener noreferrer">UPOST (in Progress)</a></h3>
 											<h3><i className="icon-github"></i> <a href ="https://github.com/willisc92/upost" target="_blank" rel="noopener noreferrer">Source Code</a></h3>
 											<small>A personalizable event service platform for the University of Calgary that increases event awareness, while providing an incentivized engagement strategy to eliminate event food waste on campus.</small>
@@ -29,22 +30,22 @@ export default class Projects extends Component {
 									</div>
 								</div>
 							</div>
-							{/* <div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/tesla.PNG)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 02</a></h3>
 											<span>Animation</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
+											<h3><i className="icon-chrome"></i> <a href="https://willisc92.github.io/Tesla-Visualization/" target="_blank" rel="noopener noreferrer">Tesla D3 Visualization</a></h3>
+											<h3><i className="icon-github"></i> <a href ="https://github.com/willisc92/Tesla-Visualization" target="_blank" rel="noopener noreferrer">Source Code</a></h3>
+											<small>A D3 visualization to visualize the sentiment regarding Tesla around the world over a couple of months.</small>
+												{/* <span><a href="#"><i className="icon-share3" /></a></span>
 												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span> */}
 										</div>
 									</div>
 								</div>
 							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+							{/* <div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
 								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
 									<div className="desc">
 										<div className="con">
