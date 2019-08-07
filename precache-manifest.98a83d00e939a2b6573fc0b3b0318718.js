cd19d7a811b314a533420aa8f3a1d5d98b8577fe
Willis Cheung <willis.cheung88@gmail.com>
2019-08-06 21:16:50 -0600

    Updates

--- precache-manifest.98a83d00e939a2b6573fc0b3b0318718.js
+++ precache-manifest.98a83d00e939a2b6573fc0b3b0318718.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "106640de9de81dba71e6",
-    "url": "/portfolio/static/css/main.0aa072fc.chunk.css"
-  },
-  {
-    "revision": "106640de9de81dba71e6",
-    "url": "/portfolio/static/js/main.106640de.chunk.js"
-  },
-  {
     "revision": "0fad4c0f10d57b530fe4",
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "3f44051ba32651285d31",
+    "url": "/portfolio/static/js/main.3f44051b.chunk.js"
   },
   {
     "revision": "5ec21d738fba382f2658",
     "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "f41319402a96949b25fdca3d875e2808",
+    "revision": "3f44051ba32651285d31",
+    "url": "/portfolio/static/css/main.0aa072fc.chunk.css"
+  },
+  {
+    "revision": "4e429f82ff491fa1894aedbddd4adb4c",
     "url": "/portfolio/index.html"
   }
 ];
